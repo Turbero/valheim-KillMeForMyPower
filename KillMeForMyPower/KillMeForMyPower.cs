bd3ed18f10518aa7afd655c35c55ee0f4d610311
Turbero <dicon18@gmail.com>
2025-09-13 02:35:38 +0200

    Updating to version 1.2.1

--- KillMeForMyPower/KillMeForMyPower.cs
+++ KillMeForMyPower/KillMeForMyPower.cs
@@ -8,7 +8,7 @@ namespace KillMeForMyPower
     {
         public const string GUID = "Turbero.KillMeForMyPower";
         public const string NAME = "Kill Me For My Power";
-        public const string VERSION = "1.2.0";
+        public const string VERSION = "1.2.1";
 
         private readonly Harmony harmony = new Harmony(GUID);
 
