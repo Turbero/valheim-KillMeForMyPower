31f4adb3e7125235ce109d0592cd28fbc65fda05
Turbero <dicon18@gmail.com>
2025-11-13 21:22:24 +0100

    Adding dungeon restrictions to interaction with cubes

--- KillMeForMyPower/Restrictions/DungeonRestriction.cs
+++ KillMeForMyPower/Restrictions/DungeonRestriction.cs
@@ -6,28 +6,58 @@ using UnityEngine;
 
 namespace KillMeForMyPower.Restrictions
 {
-    [HarmonyPatch(typeof(Teleport), "OnTriggerEnter")]
-    [HarmonyPriority(Priority.VeryHigh)]
-    public class Teleport_OnTriggerEnter_Patch
+    public class DungeonRestriction
     {
-        [HarmonyPrefix]
-        public static bool Prefix(Teleport __instance, Collider collider)
+        [HarmonyPatch(typeof(Teleport), "OnTriggerEnter")]
+        [HarmonyPriority(Priority.VeryHigh)]
+        public class Teleport_OnTriggerEnter_Patch
         {
-            if (!ConfigurationFile.restrictEnteringDungeonsBeforeKillingBosses.Value) return true;
-            
-            Logger.Log("[Teleport - OnTriggerEnter] entered");
-            string targetName = __instance.name.Replace("(Clone)", "").ToLower();
-            Logger.Log($"[Teleport - OnTriggerEnter] Trying to teleport the player through {targetName}");
-            var player = collider.GetComponentInParent<Player>();
-            if (player == null) return true;
-
-            if (!ConfigurationFile.restrictEnteringDungeonsBeforeKillingBossesAdmins.Value && GameManager.IsAdmin(player))
+            [HarmonyPrefix]
+            public static bool Prefix(Teleport __instance, Collider collider)
             {
-                Logger.Log("Player "+player.GetPlayerName()+" is admin. Skipping restriction to enter...");
-                return true;
+                if (!ConfigurationFile.restrictEnteringDungeonsBeforeKillingBosses.Value) return true;
+
+                Logger.Log("[Teleport - OnTriggerEnter] entered");
+                string targetEntranceName = __instance.name.Replace("(Clone)", "").ToLower();
+                Logger.Log($"[Teleport - OnTriggerEnter] Trying to teleport the player through {targetEntranceName}");
+                var player = collider.GetComponentInParent<Player>();
+                if (player == null) return true;
+
+                if (!ConfigurationFile.restrictEnteringDungeonsBeforeKillingBossesAdmins.Value &&
+                    GameManager.IsAdmin(player))
+                {
+                    Logger.Log("Player " + player.GetPlayerName() + " is admin. Skipping restriction to enter...");
+                    return true;
+                }
+
+                return canCrossEntrance(targetEntranceName, player);
             }
-            
-            if (targetName == "gateway")
+        }
+
+        [HarmonyPatch(typeof(Player), "Interact")]
+        public class DetectDungeonInteractPatch
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(GameObject go, Player __instance)
+            {
+                if (go == null) return true;
+                if (go.name != "Cube") return true;
+                
+                Logger.Log("[Player - Interact] Cube touched");
+                
+                if (!ConfigurationFile.restrictEnteringDungeonsBeforeKillingBossesAdmins.Value && GameManager.IsAdmin(__instance))
+                {
+                    Logger.Log("Player " + __instance.GetPlayerName() + " is admin. Skipping restriction to enter...");
+                    return true;
+                }
+
+                return canCrossEntrance(__instance.InInterior() ? "exteriorgateway" : "gateway", __instance);
+            }
+        }
+
+        private static bool canCrossEntrance(string targetEntranceName, Player player)
+        {
+            if (targetEntranceName == "gateway")
             {
                 if (player.GetCurrentBiome() == Heightmap.Biome.BlackForest && !KillMeForMyPowerUtils.HasDefeatedBossName(BossNameEnum.Eikthyr))
                     return negateAccess(player, BossNameEnum.Eikthyr);
@@ -41,7 +71,7 @@ namespace KillMeForMyPower.Restrictions
                     return negateAccess(player, BossNameEnum.Queen);
             }
 
-            if (ConfigurationFile.restrictExitingQueenDungeonIfAlive.Value && targetName == "exteriorgateway" && IsQueenNearbyAndAlert(player))
+            if (ConfigurationFile.restrictExitingQueenDungeonIfAlive.Value && targetEntranceName == "exteriorgateway" && IsQueenNearbyAndAlert(player))
             {
                 player.Message(MessageHud.MessageType.Center, ConfigurationFile.restrictExitingQueenDungeonIfAliveMessage.Value);
                 Effects.scareEffect();
@@ -58,21 +88,25 @@ namespace KillMeForMyPower.Restrictions
             Effects.scareEffect();
             return false;
         }
-        
+
         public static bool IsQueenNearbyAndAlert(Player player)
         {
             Vector3 playerPos = player.transform.position;
 
             foreach (Character c in Character.GetAllCharacters())
             {
-                if (c != null && !c.IsDead() && c.name.StartsWith("SeekerQueen", StringComparison.OrdinalIgnoreCase))
+                if (c != null && !c.IsDead() &&
+                    c.name.StartsWith("SeekerQueen", StringComparison.OrdinalIgnoreCase))
                 {
-                    if (!c.GetComponent<BaseAI>().IsAlerted())
+                    bool isAlerted = c.GetComponent<BaseAI>().IsAlerted();
+                    bool isBossActive = Location.IsInsideActiveBossDungeon(playerPos);
+                    Logger.Log($"isAlerted {isAlerted}, isBossActive {isBossActive}");
+                    if (!isAlerted && !isBossActive)
                     {
-                        Logger.LogInfo("SeekerQueen is close but not alerted. Lucky!");
+                        Logger.LogInfo("SeekerQueen is neither close nor active. Lucky!");
                         return false;
-                    } 
-                    
+                    }
+
                     float distance = Vector3.Distance(playerPos, c.transform.position);
                     Logger.Log("SeekerQueen is " + distance + " meters away!");
                     if (distance <= 60f) //60 meters is the height of the infested citadel (approx.)
